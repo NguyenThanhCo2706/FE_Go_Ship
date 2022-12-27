d6c844a9ee68e05be36d57c259cf5f25e64f1688
nguyenthanhco <conguyen.270601@gmail.com>
2022-12-27 16:36:57 +0700

    fix: change travel to js

--- src/Component/New/Travelaaa.tsx
+++ src/Component/New/Travelaaa.tsx
@@ -12,7 +12,7 @@ import { generateImageMarker } from '../../utils/generalMarker';
 import MessageBox from '../Commons/MessageBox';
 
 // eslint-disable-next-line import/no-webpack-loader-syntax
-mapboxgl.workerClass = require("worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker").default;
+// mapboxgl.workerClass = require("worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker").default;
 
 mapboxgl.accessToken = MAPBOX_ACCESS_TOKEN;
 
